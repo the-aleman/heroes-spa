52faf580bac0be224fb629bc88e450d5d7d0c611
Uriel Londoño <urielalejandro.lp@gmail.com>
2023-03-01 23:33:02 -0500

    Creación proyecto

---
+++ src/heroes/routes/HeroesRoutes.jsx
@@ -0,0 +1,15 @@
+import { Navbar } from "../../ui"
+
+export const HeroesRoutes = () => {
+    return (
+        <>
+            <Navbar/>
+            <Routes>
+                <Route path="marvel" element={ <MarvelPage/> } />
+                <Route path="dc" element={ <DcPage/> } />
+
+                <Route path="/" element={ <Navigate to="marvel"/> }/>
+            </Routes>
+        </>
+    )
+}
---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,16 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+
+import { DcPage, MarvelPage } from "../heroes";
+import { LoginPage } from "../auth";
+import { Navbar } from "../ui";
+
+export const AppRouter = () => {
+    return (
+        <>
+            <Navbar/>
+            <Routes>
+                <Route path="login" element={ <LoginPage/> } />
+            </Routes>
+        </>
+    )
+}
