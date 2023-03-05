864372c099fcd8fe569ba90d5f4b49c513ef2610
Uriel Londoño <urielalejandro.lp@gmail.com>
2023-03-04 23:19:15 -0500

    Creación de páginas para navegación del enrutamiento

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,10 +1,14 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 
 export const Navbar = () => {
 
+    const navigate = useNavigate();
+
     const onLogout = () => {
-        console.log('Logout');
+        navigate('/login', {
+            replace: true,
+        });
     }
     
     return (
