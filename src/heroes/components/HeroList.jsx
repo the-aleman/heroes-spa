6aed62df0cbb93a49d250d5b425e68a9c7806c80
Uriel Londoño <urielalejandro.lp@gmail.com>
2023-03-05 23:13:13 -0500

    Se agrega funcionalidad de listado de superheroes con detalle

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,19 +1,18 @@
 import React from 'react'
 import { getHeroesByPublisher } from '../helpers'
+import { HeroCard } from './';
 
 export const HeroList = ({ publisher }) => {
 
     const heroes = getHeroesByPublisher( publisher );
     
     return (
-        <ul>
+        <div className='row row-cols-1 row-cols-md-3 g-3'>
             { heroes.map( hero => 
                 (
-                    <li key={ hero.id }>
-                        { hero.superhero }
-                    </li>
+                    <HeroCard key={ hero.id } { ...hero } />
                 )
             ) }
-        </ul>
+        </div>
     )
 }
--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,6 +1,41 @@
+import { Navigate, useParams } from "react-router-dom";
+import { getHeroById } from "../helpers";
 
 export const HeroPage = () => {
+
+    const { id } = useParams();
+    const hero = getHeroById( id );
+
+    if ( !hero ) {
+        return <Navigate to="/"/>;
+    }
+    
     return (
-        <h1>HeroPage</h1>
+        <div className="row mt-5">
+            <div className="col-4">
+                <img
+                    src={ `../assets/heroes/${ id }.jpg` }
+                    alt={ hero.superhero }
+                    className="img-thumbnail animate__animated animate__fadeInLeft"
+                />
+            </div>
+            <div className="col-8 animate__animated animate__fadeIn">
+                <h3>{ hero.superhero }</h3>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item"><b>Alter ego: </b>{ hero.alter_ego }</li>
+                    <li className="list-group-item"><b>Publisher: </b>{ hero.publisher }</li>
+                    <li className="list-group-item"><b>First appearance: </b>{ hero.first_appearance }</li>
+                </ul>
+                <h5>Characters</h5>
+                <p>{ hero.characters }</p>
+                <button
+                    className="btn btn-outline-info"
+                    onClick={ () => window.history.back() }
+                >
+                    Regresar
+                </button>
+            </div> 
+
+        </div>
     )
 }
