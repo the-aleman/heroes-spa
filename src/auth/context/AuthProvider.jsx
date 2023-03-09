11054f2aad5cf00a6466ed584cec8c40ae5ffda6
Uriel Londoño <urielalejandro.lp@gmail.com>
2023-03-08 23:37:02 -0500

    se crea protección a ruta 'autenticación'

---
+++ src/auth/context/AuthProvider.jsx
@@ -0,0 +1,30 @@
+import { useReducer } from 'react'
+import { AuthContext } from './AuthContext'
+import { authReducer } from './authReducer'
+import { types } from '../types/types'
+
+const initialState = {
+  logged: false
+}
+
+export const AuthProvider = ({ children }) => {
+  const [authState, dispatch] = useReducer(authReducer, initialState)
+
+  const login = (name = '') => {
+    const action = {
+      type: types.login,
+      payload: {
+        id: 'ABC',
+        name
+      }
+    }
+
+    dispatch(action)
+  }
+
+  return (
+    <AuthContext.Provider value={{ ...authState, login }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,14 +1,14 @@
-import { Route, Routes } from "react-router-dom";
-import { HeroesRoutes } from "../heroes";
-import { LoginPage } from "../auth";
+import { Route, Routes } from 'react-router-dom'
+import { HeroesRoutes } from '../heroes'
+import { LoginPage } from '../auth'
 
 export const AppRouter = () => {
-    return (
-        <>
-            <Routes>
-                <Route path="login" element={ <LoginPage/> } />
-                <Route path="/*" element={ <HeroesRoutes/> } />
-            </Routes>
-        </>
-    )
+  return (
+    <>
+      <Routes>
+        <Route path='login' element={<LoginPage />} />
+        <Route path='/*' element={<HeroesRoutes />} />
+      </Routes>
+    </>
+  )
 }
